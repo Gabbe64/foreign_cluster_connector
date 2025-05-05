69c5f3dcafa5be58409e06f83a924f7d0a5ded87
scal110 <s292604@studenti.polito.it>
2025-05-05 14:29:18 +0200

    added check exists

--- liqo-shorcut/cmd/foreignclusters.go
+++ liqo-shorcut/cmd/foreignclusters.go
@@ -6,12 +6,17 @@ import (
 	"os"
 
 	liqov1beta1 "github.com/liqotech/liqo/apis/core/v1beta1"
+	"github.com/liqotech/liqo/pkg/consts"
 	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+// ---------------------------
+// LIST FOREIGNCLUSTERS
+// ---------------------------
 var foreignClustersCmd = &cobra.Command{
 	Use:   "foreignclusters",
 	Short: "List all ForeignClusters in the current Kubernetes cluster",
@@ -23,32 +28,24 @@ var foreignClustersCmd = &cobra.Command{
 	},
 }
 
-func init() {
-	rootCmd.AddCommand(foreignClustersCmd)
-}
-
 func listForeignClusters() error {
 	ctx := context.Background()
 
-	// Recupera la configurazione K8s
 	cfg, err := ctrl.GetConfig()
 	if err != nil {
 		return fmt.Errorf("unable to get kubeconfig: %w", err)
 	}
 
-	// Registra lo schema Liqo
 	scheme := runtime.NewScheme()
 	if err := liqov1beta1.AddToScheme(scheme); err != nil {
 		return fmt.Errorf("unable to add Liqo schema: %w", err)
 	}
 
-	// Crea il client
 	cl, err := client.New(cfg, client.Options{Scheme: scheme})
 	if err != nil {
 		return fmt.Errorf("unable to create client: %w", err)
 	}
 
-	// Lista i ForeignClusters
 	var fcList liqov1beta1.ForeignClusterList
 	if err := cl.List(ctx, &fcList); err != nil {
 		return fmt.Errorf("unable to list ForeignClusters: %w", err)
@@ -59,11 +56,78 @@ func listForeignClusters() error {
 		return nil
 	}
 
-	// Stampa i risultati
 	for _, fc := range fcList.Items {
 		fmt.Printf("- Name: %s\n  ClusterID: %s\n  Namespace: %s\n",
-			fc.Name, fc.Spec.ClusterIdentity.ClusterID, fc.Namespace)
+			fc.Name, fc.Spec.ClusterID, fc.Namespace)
 	}
 
 	return nil
+}
+
+// ---------------------------
+// CHECK IF A FOREIGNCLUSTER EXISTS
+// ---------------------------
+var clusterID string
+
+var checkForeignClusterCmd = &cobra.Command{
+	Use:   "foreigncluster-exists",
+	Short: "Check if a ForeignCluster with a given ClusterID exists",
+	Run: func(cmd *cobra.Command, args []string) {
+		exists, name, err := foreignClusterExists(clusterID)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			os.Exit(1)
+		}
+		if exists {
+			fmt.Printf("✅ ForeignCluster with ClusterID '%s' exists (Name: %s).\n", clusterID, name)
+		} else {
+			fmt.Printf("❌ ForeignCluster with ClusterID '%s' does NOT exist.\n", clusterID)
+		}
+	},
+}
+
+func foreignClusterExists(clusterID string) (bool, string, error) {
+	ctx := context.Background()
+
+	cfg, err := ctrl.GetConfig()
+	if err != nil {
+		return false, "", fmt.Errorf("unable to get kubeconfig: %w", err)
+	}
+
+	scheme := runtime.NewScheme()
+	if err := liqov1beta1.AddToScheme(scheme); err != nil {
+		return false, "", fmt.Errorf("unable to add Liqo schema: %w", err)
+	}
+
+	cl, err := client.New(cfg, client.Options{Scheme: scheme})
+	if err != nil {
+		return false, "", fmt.Errorf("unable to create client: %w", err)
+	}
+
+	labelSelector := labels.SelectorFromSet(labels.Set{
+		consts.RemoteClusterID: clusterID,
+	})
+
+	var fcList liqov1beta1.ForeignClusterList
+	if err := cl.List(ctx, &fcList, &client.ListOptions{
+		LabelSelector: labelSelector,
+	}); err != nil {
+		return false, "", fmt.Errorf("unable to list ForeignClusters: %w", err)
+	}
+
+	if len(fcList.Items) > 0 {
+		return true, fcList.Items[0].Name, nil
+	}
+	return false, "", nil
+}
+
+// ---------------------------
+// INIT
+// ---------------------------
+func init() {
+	rootCmd.AddCommand(foreignClustersCmd)
+
+	checkForeignClusterCmd.Flags().StringVar(&clusterID, "id", "", "ClusterID of the ForeignCluster to check")
+	checkForeignClusterCmd.MarkFlagRequired("id")
+	rootCmd.AddCommand(checkForeignClusterCmd)
 }
